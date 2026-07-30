94f0ed4709e27093bf51259440aec562a772cc74
Eric <ericj6215@gmail.com>
2026-07-30 15:39:34 +0900

    commit

---
+++ FE/washingSymbol/symbol.js
@@ -0,0 +1,11 @@
+getAllLocalData();
+
+function 기호출력(){
+    getLocalsymbol()
+    for(i=0; i<=washingSymbolList.length;i++){
+        let html =''
+        symbol=washingSymbolList[i];
+        html += `<div><img src="/FE/assets/세탁기호/img_세탁기호 1.png"/> </div>`
+    }
+    document.querySelector("div")
+}
