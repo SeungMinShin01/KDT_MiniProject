99b1faf0d4f2e1c67a392419607233c4a9b955e8
Seungminshin01 <harry9096@gmail.com>
2026-07-30 15:22:27 +0900

    feat: 의류별 세탁법 js 추가

--- FE/washingGuide/washingGuide.js
+++ FE/washingGuide/washingGuide.js
@@ -1,1 +1,19 @@
-getAllLocalData();
+printClothesCard();
+function printClothesCard() {
+  getAllLocalData();
+  let html = "";
+  let card = document.querySelector(".cloth-grid");
+  console.log(card);
+  for (let i = 0; i < clothesList.length; i++) {
+    let object = clothesList[i];
+    html += ` <div class="cloth-card">
+              <img src=".${object.imagePath}" />
+              <h4>${object.clothesName}span>(면)</span></h4>
+              <div class="badge-wrap">
+                <span class="badge badge-green">30℃ 이하</span>
+                <span class="badge badge-blue">일반 세탁</span>
+              </div>
+            </div>`;
+  }
+  card.innerHTML = html;
+}
