ef7f1c3e23fffa70438b8f3c0f57378842ec4de0
yoohyun0830-ship-it <yoohyun0830@gmail.com>
2026-07-31 11:12:39 +0900

    함수수정

--- FE/dryingGuide/dryingGuide.js
+++ FE/dryingGuide/dryingGuide.js
@@ -1,7 +1,7 @@
-let selectedId = 1;
 dryPage();
 function dryPage(){
-
+    const url = new URLSearchParams(location.search);
+    let selectedId = url.get("no");
     // 선택한 의류 찾기
     for(let i = 0; i < clothesList.length; i++){
         if(clothesList[i].clothesId == selectedId){
